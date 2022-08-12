5b7401373773b7f9d2d9ed4bcb86b131f5fac234
Muksid <muksiduzzaman124@gmail.com>
2022-08-12 20:48:28 +0530

    basic array and string functions methods

---
+++ app.js
@@ -0,0 +1,106 @@
+console.log("this is a javascript");
+
+let arr = [
+  "muksid",
+  "hazbulla",
+  "hafiza",
+  4,
+  true,
+  "true",
+  45,
+  5634,
+  5,
+  37,
+  37,
+  3,
+  73,
+  7,
+  67,
+  367,
+  3,
+  73,
+  7,
+  37,
+  3,
+  7,
+  37,
+  7,
+  37,
+  3567,
+  356,
+  7,
+  3,
+  73,
+  56,
+  7,
+];
+
+// console.log(arr);
+// console.log(arr[1]);
+
+//push and pop of an array
+
+arr.push("do you love me hazbulla");
+arr.push("tt");
+// console.log(arr);
+arr.pop();
+// console.log(arr);
+
+//reversing an array
+
+// arr.reverse();
+// console.log(arr);
+
+//concatination of an array
+
+let arr2 = ["yoyo", "momo", "nunu", "koti"];
+
+// console.log(arr2);
+
+let adding = arr.concat(arr2);
+
+// console.log(adding);
+
+let arr3 = ["thuis", "is", "somtaigho", "akufghaioh", 1, 3, 6, 3, 6];
+console.log(arr3);
+// arr3.shift();
+// console.log(arr3);
+// arr3.unshift("hero ki kya bat he ma durga uske sath he ");
+// console.log(arr3);
+
+arr3.splice(2, 4);
+console.log(arr3);
+
+let fufu = arr.length;
+console.log(fufu);
+
+// console.table(arr);
+// console.clear();
+
+// console.log(fufu);
+console.warn("this is a warning ");
+
+console.clear();
+
+let text1 = "sea";
+let text2 = "food";
+
+// let result = text1.concat(" fgsdhs" + text2);
+// console.log(result);
+
+let str = "i love you a lot goru manuh";
+
+console.log(str);
+let results = str.replace("hero", "muksid");
+// console.log(results);
+
+// let results2 = str.substring(2, 8);
+// console.log(results2);
+
+let results3 = str.split(" ");
+console.log(results3);
+
+//array di kene ata calculator
+let num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let num2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// String concate
